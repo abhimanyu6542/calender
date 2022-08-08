7f305741e0700001b70da3650fc6deb975f11747
Abhimanyu Debata <107947041+abhimanyu6542@users.noreply.github.com>
2022-08-08 21:34:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -788,8 +788,3 @@ mql.addListener(function(m) {
 	}
 });
 
-//things i regret about this project:
-//1- i didnt use a design pattern !
-//2- i used date object as a string instead of date formate in notes
-//3- as the feauters progress i end up with a spagheti code !
-// FACT: it wouldn't be possible without the builtin date object "new Date()" thanks javascript !
